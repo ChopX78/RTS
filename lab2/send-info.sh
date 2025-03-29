4076ac95ce0ec063cdeeecbba889d965c43b6d9b
чопикс <148362754+ChopX78@users.noreply.github.com>
2025-03-29 18:41:25 +0300

    Update send-info.sh

--- lab2/send-info.sh
+++ lab2/send-info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 password="123"
-remote_server="valery@192.168.0.116:/home/valery/results"
+remote_server="vanya@192.168.0.110:/home/vanya/results"
 local_dir="/home/lab2/results/*"
 sshpass -p "$password" scp $local_dir "$remote_server"
 echo "Файлы успешно перенесены на сервер!"
